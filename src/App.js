b538d4e377a01295f921723c0f1001eaa6931d5a
dilsuz <dilsuz25012303@gmail.com>
2023-04-11 06:27:38 +0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import React from 'react'
-// import Clicker from './Components/Clicker'
-import { useEffect ,useRef } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
 
-  const inputRef = useRef(null)
-
-  const handleClick = () => {
-    inputRef.current.focus();
-  }
-
-  useEffect(() => {
-    inputRef.current.focus()
+  const [user, setUser] = useState('users')
+  const [data,setData] = useState([])
+  const [position,setPosition] = useState({
+    x : 0,
+    y : 0
   })
 
-  // const [isClicker,setClicker] = useState(false)
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${user}`)
+      .then(response => response.json())
+      .then(json => setData(json))
+  }, [user])
 
-  // useEffect(() => {
-  //   console.log('render',isClicker)
-
-  //   return () => console.log('hey')
-
-  // },[isClicker])
+  useEffect(() => {
+    window.addEventListener('mousemove' , (e) => {
+      setPosition({
+        x: e.clientX,
+        y: e.clientY
+      },[])
+    })
+  })
 
   return (
     <div>
-      <h1>React App</h1>
-      {/* <button onClick={() => setClicker(!isClicker)}>Toogle Clicker</button>
-      {isClicker && <Clicker />} */}
-
-      <input 
-        type='text'
-        placeholder='Enter your name'
-        ref={inputRef}
-      />
-      <br />
-      <button onClick={handleClick}>Click</button>
-      
+      <p>User : {user} </p>
+      <button onClick={() => setUser('users')}>Web Developer</button>
+      <button onClick={() => setUser('posts')}>Android Developer</button>
+      <button onClick={() => setUser('todos')}>Ios Developer</button>
+      <p>{JSON.stringify(position)}</p>
     </div>
   )
 }
--- src/Components/Clicker.js
+++ src/Components/Clicker.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import { useState } from 'react';
 
-const Clicker = () => {
+function randomNumber() {
+  console.log('run...');
+  return Math.trunc(Math.random() * 20)
+}
 
-    const [state,setState] = useState(0)
+
+const Clicker = () => {
+  
+  function uploadObject() {
+    setDate((prev) => {
+      return{
+        ...prev,
+        text : 'New Text'
+      }
+    })
+  }
+    const [state,setState] = useState(randomNumber())
+    const [date,setDate] = useState({
+      text : 'Number',
+      date : Date.now()
+    })
+
 
   return (
     <div>
       <h2>Counter : {state}</h2>
       <button onClick={() => setState(state + 1)}>increment</button>
       <button onClick={() => setState(state - 1)}>decrement</button>
+      <button onClick={uploadObject}>Upload</button>
+      <p>{JSON.stringify(date)}</p>
     </div>
   )
 }
